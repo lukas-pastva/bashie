889b8a72663b75cfb79015722dd6a763af873752
Lukas Pastva <lukas.pastva>
2024-03-03 22:07:01 +0100

    feat: initial commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+
+echo ''
+echo "--------------------------------------------------------"
 echo "Parsing JSON_DATA to environment variables..."
 for row in $(echo "${JSON_DATA}" | jq -r "to_entries|map(\"\(.key)=VAR_\(.value|tostring)\")|.[]"); do
   export $row
 done
 echo "Environment variables are set."
 
-# Use a temporary file to store BASH_DATA
-echo "Storing BASH_DATA script in temporary file..."
 echo "$BASH_DATA" > /tmp/bash.sh
 
-echo "Cating BASH_DATA script..."
-cat /tmp/bash.sh
-
+echo ''
+echo "--------------------------------------------------------"
 echo "Executing BASH_DATA script..."
 bash /tmp/bash.sh
