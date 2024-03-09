6c0eb5e6a02b0d9a771ee16c4983efdf6ee7e247
Lukas Pastva <lukas.pastva>
2024-03-09 07:35:37 +0100

    feat: initial commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 echo ''
 echo "--------------------------------------------------------"
 echo "Parsing JSON_DATA to environment variables..."
-for row in $(echo "${JSON_DATA}" | jq -r "to_entries|map(\"\(.key)=VAR_\(.value|tostring)\")|.[]"); do
+for row in $(echo "${JSON_DATA}" | jq -r "to_entries|map(\"\(.key)=var_\(.value|tostring)\")|.[]"); do
   export $row
 done
 echo "Environment variables are set."
