f2139addddc0d247ee0ee95b7726b76dd14c1da5
Lukas Pastva <lukas.pastva>
2024-03-09 10:18:37 +0100

    feat: initial commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,41 +1,31 @@
 #!/bin/bash
 
-# Function to export environment variables from JSON_DATA
-export_env_variables() {
-  echo ""
-  echo "--------------------------------------------------------"
-  echo "Parsing JSON_DATA to environment variables..."
-  if [[ -n "${JSON_DATA}" ]]; then
-    while IFS="=" read -r key value; do
-      # Export environment variables with proper quoting
-      export "$key"="$value"
-    done < <(echo "${JSON_DATA}" | jq -r 'to_entries|map("var_\(.key)=\(.value|tostring)")|.[]' 2> /dev/null)
+echo ""
+echo "--------------------------------------------------------"
+echo "Parsing JSON_DATA to environment variables..."
+# Only proceed if JSON_DATA is not empty
+if [[ -n "${JSON_DATA}" ]]; then
+  # Parsing JSON_DATA and prefixing variable names with "var_"
+  while IFS="=" read -r name value; do
+    # Using eval to handle complex scenarios safely
+    eval "export var_${name}='${value}'"
+  done < <(echo "${JSON_DATA}" | jq -r 'to_entries | map("\(.key)=\(.value | tostring)") | .[]')
+  echo "Environment variables are set."
+else
+  echo "JSON_DATA is empty. Skipping setting environment variables."
+fi
+echo "--------------------------------------------------------"
 
-    if [ $? -ne 0 ]; then
-        echo "Error parsing JSON_DATA with jq."
-        exit 1
-    fi
 
-    echo "Environment variables are set."
-  else
-    echo "JSON_DATA is empty. Skipping environment variables setup."
-  fi
-  echo "--------------------------------------------------------"
-}
-
-# Function to execute BASH_DATA script
-execute_bash_data() {
-  echo ""
-  echo "--------------------------------------------------------"
-  echo "Executing BASH_DATA script..."
-  if [[ -n "${BASH_DATA}" ]]; then
-    bash -c "${BASH_DATA}"
-  else
-    echo "BASH_DATA is empty. Skipping script execution."
-  fi
-  echo "--------------------------------------------------------"
-}
-
-# Main script execution
-export_env_variables
-execute_bash_data
+echo ""
+echo "--------------------------------------------------------"
+echo "Executing BASH_DATA script..."
+# Only proceed if BASH_DATA is not empty
+if [[ -n "${BASH_DATA}" ]]; then
+  # Directly execute BASH_DATA content
+  bash -c "${BASH_DATA}"
+  echo "BASH_DATA script executed."
+else
+  echo "BASH_DATA is empty. Skipping script execution."
+fi
+echo "--------------------------------------------------------"
