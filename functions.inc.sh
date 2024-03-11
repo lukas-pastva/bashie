f9b4ad4c077cbaab189128ceb39dfa94d0f82b1d
Lukas Pastva <lukas.pastva>
2024-03-11 21:45:47 +0100

    feat: initial commit

--- functions.inc.sh
+++ functions.inc.sh
@@ -65,3 +65,184 @@ function edit_file_on_git() {
     echo "---> Contents already exist in the file. No changes made."
   fi
 }
+
+# Function to clone GitLab group repositories, including subgroups, maintaining hierarchy
+gitlab_backup() {
+  if [[ ${gitlab_private_token} == "" ]]; then
+      echo "Enter your GitLab Private Token: "
+      read gitlab_private_token
+      export gitlab_private_token="${gitlab_private_token}"
+  fi
+
+  if [[ ${group_id} == "" ]]; then
+      echo "Enter your GitLab Group ID: "
+      read group_id
+      export group_id="${group_id}"
+  fi
+
+  local parent_path=$(pwd)
+
+  # Function to backup project variables
+  _backup_variables() {
+      local project_id=$1
+      local backup_dir=$2
+      echo "Backing up variables for project ID $project_id"
+      curl --silent --header "PRIVATE-TOKEN: $gitlab_private_token" \
+          "https://gitlab.com/api/v4/projects/$project_id/variables" > "$backup_dir/variables.json"
+  }
+
+  # Function to backup project issues and comments
+  _backup_issues() {
+      local project_id=$1
+      local backup_dir=$2
+      echo "Backing up issues and comments for project ID $project_id"
+
+      # Fetch issues for the project
+      local issues_response=$(curl --silent --header "PRIVATE-TOKEN: $gitlab_private_token" \
+          "https://gitlab.com/api/v4/projects/$project_id/issues?with_labels_details=true&include_subscribed=true&per_page=100")
+
+      # Check if there are issues available
+      if [[ -z "$issues_response" || "$issues_response" == "[]" ]]; then
+          echo "No issues found for project ID $project_id."
+          return
+      fi
+
+      # Iterate through each issue and fetch its comments
+      echo "$issues_response" | jq -c '.[]' | while IFS= read -r issue; do
+          local issue_iid=$(echo "$issue" | jq -r '.iid')
+
+          # Fetch comments for the issue
+          local issue_comments_response=$(curl --silent --header "PRIVATE-TOKEN: $gitlab_private_token" \
+              "https://gitlab.com/api/v4/projects/$project_id/issues/$issue_iid/notes")
+
+          # Check if comments were fetched successfully
+          if [[ -n "$issue_comments_response" && "$issue_comments_response" != "[]" ]]; then
+              # Save comments to a file
+              echo "$issue_comments_response" > "$backup_dir/issue_${issue_iid}_comments.json"
+          else
+              echo "No comments found for issue IID $issue_iid in project ID $project_id."
+          fi
+      done
+
+  }
+
+  # Function to backup CI/CD settings for a group
+  _backup_cicd_settings() {
+      local group_id=$1
+      local backup_dir=$2
+      echo "Backing up CI/CD settings for group ID $group_id"
+      curl --silent --header "PRIVATE-TOKEN: $gitlab_private_token" \
+          "https://gitlab.com/api/v4/groups/$group_id/ci/pipeline_schedules" > "$backup_dir/cicd_pipeline_schedules.json"
+  }
+
+  # Function to backup group issues
+  _backup_group_issues() {
+      local group_id=$1
+      local backup_dir=$2
+      echo "Backing up issues for group ID $group_id"
+      curl --silent --header "PRIVATE-TOKEN: $gitlab_private_token" \
+          "https://gitlab.com/api/v4/groups/$group_id/issues" > "$backup_dir/group_issues.json"
+  }
+
+  # New function to backup CI/CD variables for a group
+  _backup_group_variables() {
+      local group_id=$1
+      local backup_dir=$2
+      echo "Backing up CI/CD variables for group ID $group_id"
+      curl --silent --header "PRIVATE-TOKEN: $gitlab_private_token" \
+          "https://gitlab.com/api/v4/groups/$group_id/variables" > "$backup_dir/group_variables.json"
+  }
+
+  # Function to zip and clean up the backup directories
+  _zip_and_cleanup() {
+      local backup_root_dir=$1  # The root directory where all backups are stored
+
+      echo "Compressing backup directories into a single archive..."
+      # Creating a ZIP file for the entire backup directory
+      # Note: Adjust the zip command according to your needs or system compatibility
+      zip -r "${backup_root_dir}/gitlab_backup_$(date +%Y-%m-%d_%H-%M-%S).zip" "$backup_root_dir" -x "*.zip"
+
+      echo "Removing original backup directories..."
+      # Find and delete the original directories but keep the zip file
+      find "$backup_root_dir" -mindepth 1 -maxdepth 1 ! -name "*.zip" -exec rm -rf {} +
+
+      echo "Backup and cleanup process completed."
+  }
+
+  # Recursive function to clone projects and handle subgroups, including variables, issues, CI/CD settings, and CI/CD variables backup
+  _clone_recursive() {
+      local current_group_id=$1
+      local parent_dir=$2
+
+      # Backup CI/CD settings, CI/CD variables, and issues for the current group
+      _backup_cicd_settings "$current_group_id" "$parent_dir"
+      _backup_group_issues "$current_group_id" "$parent_dir"
+      _backup_group_variables "$current_group_id" "$parent_dir" # Call to backup group CI/CD variables
+
+      local page=1
+      while : ; do
+          local projects_response=$(curl --silent --header "PRIVATE-TOKEN: $gitlab_private_token" \
+              "https://gitlab.com/api/v4/groups/$current_group_id/projects?include_subgroups=true&per_page=100&page=$page")
+          local project_count=$(echo "$projects_response" | jq '. | length')
+          if [[ "$project_count" -eq 0 ]]; then break; fi
+
+          echo "$projects_response" | jq -c '.[]' | while read project; do
+              local project_id=$(echo $project | jq -r '.id')
+              local project_path=$(echo $project | jq -r '.path_with_namespace')
+              local http_url_to_repo=$(echo $project | jq -r '.http_url_to_repo')
+              local clone_dir="$parent_dir/${project_path}"
+
+              local modified_clone_url=$(echo "$http_url_to_repo" | sed "s|https://|https://user:$gitlab_private_token@|")
+
+              # Cloning the regular repository
+              if [ -d "$clone_dir" ] && [ "$(ls -A $clone_dir)" ]; then
+                  echo "Directory $clone_dir already exists and is not empty. Attempting to pull latest changes."
+                  (cd "$clone_dir" && git pull)
+              else
+                  echo "Cloning all branches of $modified_clone_url into $clone_dir"
+                  mkdir -p "$clone_dir" && git clone "$modified_clone_url" "$clone_dir"
+              fi
+
+              # Cloning the mirror repository
+              local mirror_dir="${parent_dir}/${project_path}_mirror"
+              if [ -d "$mirror_dir" ]; then
+                  echo "Mirror directory $mirror_dir already exists. Attempting to update the mirror."
+                  (cd "$mirror_dir" && git remote update)
+              else
+                  echo "Cloning a mirror of $modified_clone_url into $mirror_dir"
+                  mkdir -p "$mirror_dir" && git clone --mirror "$modified_clone_url" "$mirror_dir"
+              fi
+
+              # Backing up variables and issues
+              _backup_variables "$project_id" "$clone_dir"
+              _backup_issues "$project_id" "$clone_dir"
+          done
+
+          ((page++))
+      done
+
+      page=1
+      while : ; do
+          local subgroups_response=$(curl --silent --header "PRIVATE-TOKEN: $gitlab_private_token" \
+              "https://gitlab.com/api/v4/groups/$current_group_id/subgroups?per_page=100&page=$page")
+          local subgroup_count=$(echo "$subgroups_response" | jq '. | length')
+          if [[ "$subgroup_count" -eq 0 ]]; then break; fi
+
+          echo "$subgroups_response" | jq -c '.[]' | while read subgroup; do
+              local subgroup_id=$(echo $subgroup | jq -r '.id')
+              local subgroup_dir="$parent_dir/$(echo $subgroup | jq -r '.full_path')"
+
+              mkdir -p "$subgroup_dir"
+              _backup_group_issues "$subgroup_id" "$subgroup_dir"
+              _backup_group_variables "$subgroup_id" "$subgroup_dir" # Backup group CI/CD variables for subgroups
+
+              _clone_recursive "$subgroup_id" "$parent_dir"
+          done
+
+          ((page++))
+      done
+  }
+
+  _clone_recursive "$group_id" "$parent_path"
+  _zip_and_cleanup "$parent_path"
+}
